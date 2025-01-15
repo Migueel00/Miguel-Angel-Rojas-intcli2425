e0d9540a681e9f47f67bacf0030cc16796de660e
Migueel00 <rojassoriamiguelangel95@gmail.com>
2025-01-15 12:28:25 +0100

    [MAIN][NEW]List of potions horizontal

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { potions } from './data/data'
+import { Potion } from './types/Potion'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [showingPotions, setShowingPotions] = useState<Potion[]>(potions);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='w-[100%] min-h-screen flex justify-center '>
+        <div className='w-[98%] flex flex-wrap justify-center items-center'>
+          { showingPotions.map(potion =>  
+            <Card potion={potion}></Card>
+          )}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,26 @@
+import { Potion } from "../types/Potion";
+interface CardProps {
+    potion: Potion;
+}
+
+
+const Card : React.FC<CardProps>  = (potion) => {
+    return ( 
+        <div className="w-[18%] h-[38%] ml-1 flex items-center flex-col rounded-md shadow-xl bg-gray-200">
+            <div className="w-full">
+                <img className="object-cover w-full h-56 rounded-md" src={potion.potion.image} alt="Converse sneakers" />
+            </div>
+            <p className="text-center mt-2 text-orange-400 font-bold">{potion.potion.name}</p>
+            <div className="flex flex-row mt-2">
+                <div>
+                    <p><span className="font-bold">Rarity:</span> {potion.potion.rarity}</p>
+                    <p><span className="font-bold">Boss:</span> {potion.potion.meta.availability.drop_rate.boss}</p>
+                    <p><span className="font-bold">Chance:</span> {potion.potion.meta.availability.drop_rate.chance}</p>
+                </div>
+                <button className="bg-orange-400 ml-6 p-2 rounded-lg w-[100px]"> Details </button>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
