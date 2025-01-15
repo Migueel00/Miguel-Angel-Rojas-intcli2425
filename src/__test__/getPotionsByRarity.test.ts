1c5e2fb43286f134499dc4cb523b85ce77b2f9ba
Migueel00 <rojassoriamiguelangel95@gmail.com>
2025-01-15 10:14:58 +0100

    [MAIN][NEW]GetPotionsByRarity function and test

---
+++ src/__test__/getPotionsByRarity.test.ts
@@ -0,0 +1,17 @@
+
+import { getPotionsByRarity } from "../helpers/potionHelpers";
+import { filterByLevelDataMock } from "./__mocks__/filterByLevelMock";
+
+describe('Test the function filterByLevel', () => {
+
+    it('Should filter by level and return an Array of potions' , () => {
+        // Assign
+
+        // Act
+        const filteredPotions = getPotionsByRarity(filterByLevelDataMock, "legendary");
+
+        // Assert
+        expect(filteredPotions.length).toBe(3);
+    });
+});
+
--- src/helpers/potionHelpers.ts
+++ src/helpers/potionHelpers.ts
@@ -3,9 +3,12 @@ import { Potion } from "../types/Potion";
 
 // Devuelve array de pociones nivel menor o igual al especificado
 export function FilterByLevelRequirement(potions : Potion[], level : number) : Potion[]{
-
     return potions.filter(potion => potion.usage.restrictions.levelRequirement <= level);
-    
 }
+
+// Devuelve todas las pociones que tiene una rareza especifica
+export function getPotionsByRarity(potions: Potion[], rarity: string) : Potion[]{ 
+    return potions.filter(potion => potion.rarity === rarity);
+}   
 
 
