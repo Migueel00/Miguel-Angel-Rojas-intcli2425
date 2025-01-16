fb4bb4e1f914072c489b94c79e7a3b0bcbdb5d46
Migueel00 <rojassoriamiguelangel95@gmail.com>
2025-01-16 09:07:38 +0100

    [MAIN][NEW]EffectFilter

---
+++ src/components/EffectFilter.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { Potion } from "../types/Potion";
+import { Dispatch, SetStateAction } from "react";
+import { findPotionByEffect } from "../helpers/potionHelpers";
+
+interface RarityFilterProps {
+    setShowingPotions: Dispatch<SetStateAction<Potion[]>>;
+    potions: Potion[];
+}
+
+const EffectFilter = ({setShowingPotions, potions} : RarityFilterProps) => {
+    const [effect, setEffect] = useState<string>("");
+
+    useEffect(() => {
+        effect === '' ? setShowingPotions(potions) :  setShowingPotions(findPotionByEffect(potions, effect));
+    }, [effect]);
+
+    const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setEffect(event.currentTarget.value);
+    }
+
+    return (
+        <div className="w-[20%]">
+            <input type="text" className="border-2 border-black ml-2" onChange={changeHandler}></input>
+            <p>El texto insertado {effect}</p>
+        </div>
+    );
+}
+
+export default EffectFilter;
